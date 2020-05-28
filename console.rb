0f3ee01c8116f39ca8152d52391c6ec7b6360252
Ronan Hughes <rhughes490@gmail.com>
2020-05-28 15:42:05 +0100

    Many to many commit

--- console.rb
+++ console.rb
@@ -4,6 +4,7 @@ require_relative('./models/star')
 
 require('pry')
 
+Casting.delete_all()
 Movie.delete_all()
 Star.delete_all()
 
@@ -30,8 +31,64 @@ Star.delete_all()
     "first_name" => "John",
     "last_name" => "Wayne"
 })
+@star1_id = @star1.save()
 
-@star1.save()
+# in longest day
+@star2 = Star.new({
+    "first_name" => "Peter",
+    "last_name" => "van Eyck"
+})
+@star2_id = @star2.save()
+
+# in stagecoach
+@star3 = Star.new({
+    "first_name" => "Claire",
+    "last_name" => "Trevor"
+})
+@star3_id = @star3.save()
+@star4 = Star.new({
+    "first_name" => "Andy",
+    "last_name" => "Devine"
+})
+@star4_id = @star4.save()
+
+##########
+
+# John Wayne in Stagecoach
+r = Casting.new({
+    "movie_id" => @movie2_id,
+    "star_id" => @star1_id,
+    "fee" => 100
+})
+r.save
+
+# John Wayne in Longest day
+Casting.new({
+    "movie_id" => @movie3_id,
+    "star_id" => @star1_id,
+    "fee" => 500
+}).save
+
+# Van Eyck in Longest day
+Casting.new({
+    "movie_id" => @movie3_id,
+    "star_id" => @star2_id,
+    "fee" => 10
+}).save
+
+# Clare Trevor in Stagecoach
+Casting.new({
+    "movie_id" => @movie2_id,
+    "star_id" => @star3_id,
+    "fee" => 10
+}).save
+# Andy Devine in Stagecoach
+Casting.new({
+    "movie_id" => @movie2_id,
+    "star_id" => @star4_id,
+    "fee" => 4
+}).save
+
 
 binding.pry
 nil
--- models/casting.rb
+++ models/casting.rb
@@ -1,3 +1,48 @@
+require_relative("../db/sql_runner")
+    
 class Casting
+
+    attr_reader :id
+    attr_accessor :movie_id, :star_id, :fee
+
+    def initialize (options)
+        @id = options['id'].to_i if options['id']
+        @movie_id = options['movie_id'].to_i
+        @star_id = options['star_id'].to_i
+        @fee = options['fee']
+    end
+
+    def save()
+        sql = "INSERT into castings
+        (
+            movie_id,
+            star_id,
+            fee
+        )    
+        VALUES
+        (
+            $1, $2, $3
+        )
+        RETURNING id"
+        values = [@movie_id, @star_id, @fee]
+        casting = SqlRunner.run(sql, values).first
+        @id = casting['id'].to_i
+    end
+
+    def Casting.all()
+        sql = "SELECT * FROM castings"
+        castings = SqlRunner.run(sql)
+        return Casting.map_items(castings)
+    end
+
+    def Casting.delete_all()
+        sql = "DELETE FROM castings"
+        castings = SqlRunner.run(sql)
+    end
+
+    def Casting.map_items(array)
+        return array.map{ |options| Casting.new(options) }
+    end
+
 
 end
